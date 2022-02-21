04a0a1c40f0db9ce35abc324373b7321126dd117
Dhanush <dhanushkrish45@gmail.com>
2022-02-21 21:39:10 +0530

    practice form design commit-4

--- src/app/user-account/register/register.component.ts
+++ src/app/user-account/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import * as moment from 'moment';
 
 @Component({
@@ -9,7 +9,10 @@ import * as moment from 'moment';
 })
 export class RegisterComponent implements OnInit {
 
+
+
   mobileNumber!: number; 
+  form: any;
   constructor() { }
   
    public ageFromDateOfBirthday(dateOfBirth: any): number {
@@ -23,6 +26,9 @@ export class RegisterComponent implements OnInit {
   hide = true;
 
   email = new FormControl('', [Validators.required, Validators.email]);
+  phone = new FormControl('', [Validators.required, Validators.nullValidator]);
+  password = new FormControl('', [Validators.required, Validators.nullValidator]);
+
 
   getErrorMessage() {
     if (this.email.hasError('required')) {
@@ -30,13 +36,27 @@ export class RegisterComponent implements OnInit {
     }
     return this.email.hasError('email') ? 'Not a valid email' : '';
   }
+  getErrorMessageForPhone() {
+    if (this.phone.hasError('required')) {
+      return 'Please Enter Mobile Number';
+    }
+    return this.phone.hasError('phone') ? 'Not a valid number' : '';
+  }
+  getErrorMessageForPassword() {
+    if (this.password.hasError('required')) {
+      return 'Password cannot be Empty!';
+    }
+    return this.password.hasError('password') ? 'Not a valid password' : '';
+  }
+
+  
   notvalid(){ 
   }
   
   date : any;
   resultPickerModel!: any;
   minDate = new Date(1800,1,1);
-  maxDate = new Date(2022,1,18);
+  maxDate = new Date();
   hasError(){
 
   }
@@ -51,4 +71,45 @@ export class RegisterComponent implements OnInit {
       event.preventDefault();
     }
   }
+
+  birthDate = 1996;
+
+  getAge(birthDate: Date): number {
+    const ageTilNowInMilliseconds = Date.now() - birthDate.getTime();
+    const ageDate = new Date(ageTilNowInMilliseconds);
+    console.log(birthDate);
+    return Math.abs(ageDate.getUTCFullYear() - 1970); // Because computers count the today date from the 1st of January 1970
+}
+
+
+
+changeTitle(inputElement: { value: number; focus: () => void; }){
+  inputElement.value = this.calc;
+  inputElement.focus();
+}
+
+val!: number;
+val2!: number;
+calc!: number;
+calc2!: number;
+yearless = 1900;
+ageCalc(){
+  if (this.val<1900) {
+    return this.yearless;
+  } else {
+    this.calc = 2022-this.val;
+    return this.calc;
+  }
+}
+ageCalc2(){
+  if (this.val2<1900) {
+    return this.yearless;
+  } else {
+    this.calc2 = 2022-this.val2;
+    return this.calc2;
+  }
+}
+
+
+ 
 }
